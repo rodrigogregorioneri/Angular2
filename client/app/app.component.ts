07fd66a2e7fe4b232c594bc3f7c97c29c1e03a48
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-09-05 08:11:09 -0300

    component inicial

---
+++ client/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'; // importação de um decorator primeiro parametro é { o que ele quer importar} e from é de onde ele quer
+
+// Decorators Component importado acima e abaixo ele é usado como anotação 
+@Component({
+
+})
+
+
+// classe ECMA 6 
+class AppComponent{
+
+    
+}
