49b93ae66296ddcd9ef0955028bccda0ba70b59d
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-09-05 09:13:47 -0300

    Configuração do modulo angular 2

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'
+import { NgModule } from '@angular/core'; // Dependencia ANGULAR 2
+import { BrowserModule } from '@angular/platform-browser'; // Dependencia que ajuda a renderizar no browser 
+import { AppComponent } from './app.component' // Componente criado na pasta app
+
+/**
+ * @NgModule 
+ * 
+ * imports são as importações e necessaria vir com BrowserModule pois ele que defini onde vai renderizar no caso browser 
+ * mas poderia ser em um app mobile ionic então poderia ser outro
+ * 
+ * declarations são os components que fazem parte desse modulos podem ter mais de um por enquanto só o AppComponent
+ * 
+ * bootstrap é responsavel por definir o componente que vai botar ou seja iniciar a aplicação nesse caso é AppComponent
+ */
 
 @NgModule({
    imports: [ BrowserModule],
-   declarations: [ AppComponent]
+   declarations: [ AppComponent],
+   bootstrap: [AppComponent]
 })
 class AppModule{
 
